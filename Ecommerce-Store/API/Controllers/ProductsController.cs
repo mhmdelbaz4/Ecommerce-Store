5a2c8139dcec89dee38fa10d37ca1d247c6869d4
Mohamed Elbaz <mhmdelbaz57@gmail.com>
2024-07-20 13:05:52 +0300

    initial commit

---
+++ Ecommerce-Store/API/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _storeContext;
+
+        public ProductsController(StoreContext storeContext)
+        {
+            this._storeContext = storeContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            IEnumerable<Product> products = _storeContext.Products;
+
+            return Ok(products);  
+        }
+    }
+}
